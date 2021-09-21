8dac204ba23f96afa4a86c7e7b2a02932b9eac8d
Sergey Tsypanov <sergeyt@backbase.com>
2021-09-21 15:51:20 +0300

    Fix module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module spring.boot.benchmark {
     requires jdk.unsupported;
     requires jmh.core;
     requires spring.boot;
+    requires spring.core;
+    requires spring.beans;
     requires spring.context;
     requires spring.boot.autoconfigure;
 
     opens com.tsypanov.sbb;
+    opens com.tsypanov.sbb.jmh_generated;
 }
